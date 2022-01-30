aa52c0c749cd22c218aec97e6dd7711f87944e98
Gaurav Shrivastava <92916362+gauravshri893@users.noreply.github.com>
2022-01-30 19:14:54 +0530

    Create firstchild.py

---
+++ firstchild.py
@@ -0,0 +1,4 @@
+#adding branches
+print("hello this is the first child branch from the main branch")
+print(" here I perform my experiments with the software I'm going to develop")
+print("I am a CFD Engineer and trying to create an open source CFD solver")x
